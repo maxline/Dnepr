4ba0c3235a38ebd46a9c32773281d52e55456985
maxline <sergml@mail.ru>
2016-03-07 22:46:21 +0200

    dnepr-project. start

---
+++ src/main/java/org/maxline/WelcomeApp.java
@@ -0,0 +1,13 @@
+package org.maxline;
+
+/**
+ * Hello world!
+ *
+ */
+public class WelcomeApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/maxline/WelcomeAppTest.java
@@ -0,0 +1,38 @@
+package org.maxline;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class WelcomeAppTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WelcomeAppTest(String testName)
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( WelcomeAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
